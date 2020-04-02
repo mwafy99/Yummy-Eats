154e2597162a32146f8d7954e843b35d6545c10d
mwafy99 <imwafy99@gmail.com>
2020-04-02 20:30:39 +0200

    JS added - final update

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 let btn = document.querySelector('#btn');
+let overlay = document.querySelector('#overlay');
 
-btn.addEventListener('click', (e) => {
-
+btn.addEventListener('click', function () {
+    overlay.style.display = 'grid';
+    overlay.classList.add('overlay-animation');
 });
