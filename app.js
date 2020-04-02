a9d1cec4550681052eab23d072e271a51b01fe65
mwafy99 <imwafy99@gmail.com>
2020-04-02 22:13:17 +0200

    final update v1.1

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 let btn = document.querySelector('#btn');
 let overlay = document.querySelector('#overlay');
 
-btn.addEventListener('click', function () {
-    overlay.style.display = 'grid';
+btn.addEventListener('click', () => {
+    // add grid to display and animations
+    overlay.style.display = "grid";
     overlay.classList.add('overlay-animation');
+
+    // set display to none after 3 seconds 
+    setTimeout(function () {
+        overlay.style.display = "none";
+    }, 3000);
 });
